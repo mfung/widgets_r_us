d67fe132a5bcedfaf9c788e82d1f971947f1c16e
Meng Fung <meng.fung@gmail.com>
2019-09-04 00:51:57 -0700

    updated delete

--- lib/widgets_r_us_web/resolvers/carts.ex
+++ lib/widgets_r_us_web/resolvers/carts.ex
@@ -2,6 +2,7 @@ defmodule WidgetsRUsWeb.Resolvers.Cart do
   alias WidgetsRUs.Sales.Cart
   alias WidgetsRUs.Repo
   alias WidgetsRUsWeb.Helpers.Errors
+  import Ecto.Changeset
 
   def list_all(_parent, _args, %{context: %{current_user: current_user}}) do
     { :ok, WidgetsRUs.Sales.list_carts(current_user) }
@@ -25,6 +26,16 @@ defmodule WidgetsRUsWeb.Resolvers.Cart do
   end
 
   def create_cart(_parent, _args, _resolution) do
+    {:error, "Not Authorized"}
+  end
+
+  def delete_cart(_parent, %{id: id}, %{context: %{current_user: current_user}}) do
+    cart = Repo.get(Cart, id)
+
+    WidgetsRUs.Sales.delete_cart(cart)
+  end
+
+  def delete_cart(_parent, _args, _resolution) do
     {:error, "Not Authorized"}
   end
 end
